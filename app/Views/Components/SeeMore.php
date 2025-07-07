e6e6f9acd13aaa204afcac5b4ee60198121c75f3
Xavier Grangier <grangier@gmail.coml>
2025-07-07 10:53:04 +0200

    always expended

--- app/Views/Components/SeeMore.php
+++ app/Views/Components/SeeMore.php
@@ -10,13 +10,11 @@ class SeeMore extends Component
 {
     public function render(): string
     {
-        $seeMoreLabel = lang('Common.see_more');
-        $seeLessLabel = lang('Common.see_less');
+        // SeeMore toujours expanded - pas de toggle "voir plus/voir moins"
         return <<<HTML
             <div class="see-more" style="--content-height: 10rem">
-                <input id="see-more-checkbox" type="checkbox" class="see-more__checkbox" aria-hidden="true">
+                <input id="see-more-checkbox" type="checkbox" class="see-more__checkbox" checked aria-hidden="true">
                 <div class="mb-2 see-more__content {$this->class}"><div class="see-more_content-fade"></div>{$this->slot}</div>
-                <label for="see-more-checkbox" class="see-more__label" data-see-more="{$seeMoreLabel}" data-see-less="{$seeLessLabel}" aria-hidden="true"></label>
             </div>
         HTML;
     }
